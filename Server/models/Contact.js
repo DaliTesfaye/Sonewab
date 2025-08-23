174725bcc9a62af2215b3df53a6f7a43e28dc2cd
DaliTesfaye <drididali93@gmail.com>
2025-08-23 20:29:19 +0100

    Contact Updated

--- Server/models/Contact.js
+++ Server/models/Contact.js
@@ -2,7 +2,3 @@ const mongoose = require("mongoose");
 const contactSchema = require("./schemas/ContactSchema"); 
 
 module.exports = mongoose.model("Contact", contactSchema);
-const mongoose = require("mongoose");
-const contactSchema = require("./schemas/ContactSchema"); // ✅ import du vrai schema
-
-module.exports = mongoose.model("Contact", contactSchema);
